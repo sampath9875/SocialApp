1babd650aa7badd102887255bcd665ec9dd3a709
azureUser2 <azureUser2@TestingVMImage.fyxwoovprb1evgdvkufpc4kjwf.ix.internal.cloudapp.net>
2016-10-22 15:06:08 +0530

    changed spring dependencies

--- src/com/mindtree/socialapp/controller/SampleController.java
+++ src/com/mindtree/socialapp/controller/SampleController.java
@@ -3,10 +3,16 @@
  */
 package com.mindtree.socialapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * @author M1030496
  *
  */
+
+@Controller
 public class SampleController {
 
 	/**
@@ -14,6 +20,11 @@ public class SampleController {
 	 */
 	public SampleController() {
 	}
-
+	
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String home()
+	{
+		return "registration";
+	}
 	
 }
